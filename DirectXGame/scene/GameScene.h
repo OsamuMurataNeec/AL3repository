8ffa7bfe34624a72e697728dd4e71ba01ed93bad
OsamuMurataNeec <murataosa@gmail.com>
2024-04-09 15:58:16 +0900

    01-02エンジンの使い方

--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -8,6 +8,9 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include "DebugCamera.h"
+
+
 
 /// <summary>
 /// ゲームシーン
@@ -48,4 +51,22 @@ private: // メンバ変数
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	// スプライト
+	Sprite* sprite_ = nullptr;
+	// 3Dモデル
+	Model* model_ = nullptr;
+	// ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	// ビュープロジェクション
+	ViewProjection viewProjection_;
+	// サウンドデータハンドル
+	uint32_t soundDataHandle_ = 0;
+	// 音声再生ハンドル
+	uint32_t voiceHandle_ = 0;
+	// ImGuiで値を入力する変数
+	float inputFloat3[3] = {0, 0, 0};
+	// デバッグカメラ
+	DebugCamera* debugCamera_ = nullptr;
 };
